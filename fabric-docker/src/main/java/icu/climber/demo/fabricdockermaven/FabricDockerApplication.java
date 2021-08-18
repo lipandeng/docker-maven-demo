abf1f069cec19059a72924fad09b935d192e5299
lipandeng <li.pandeng@163.com>
2021-08-18 17:54:17 +0800

    fabric-docker-maven使用

---
+++ fabric-docker/src/main/java/icu/climber/demo/fabricdockermaven/FabricDockerApplication.java
@@ -0,0 +1,13 @@
+package icu.climber.demo.fabricdockermaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FabricDockerApplication.class, args);
+    }
+
+}
---
+++ fabric-docker/src/test/java/icu/climber/demo/fabricdockermaven/FabricDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.climber.demo.fabricdockermaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
